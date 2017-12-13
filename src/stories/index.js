05d4f9209401ffe2b6b4480dbac51501d0dba981
szare <zare1356@gmail.com>
2017-12-13 17:02:02 +1100

    update .gitignore

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import {Icon} from 'fantastic-components';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to fantastic-components', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Icon', module)
+  .add('user icon', () => <Icon iconClass={'user'} foreColor={'#f00'}/>)
+  .add('calendar icon', () => <Icon iconClass={'calendar'} foreColor={'green'}/>);
